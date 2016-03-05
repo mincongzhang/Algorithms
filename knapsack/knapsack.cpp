447687ab0f8772154c23a9f97aa9de02225c0b91
Mincong <mincong.zhang@hotmail.com>
2016-03-05 20:55:45 +0000

    Update knapsack.cpp

--- knapsack/knapsack.cpp
+++ knapsack/knapsack.cpp
@@ -15,7 +15,7 @@ int c[N + 1][W + 1];
 void printc(int n, int w){
 	for (int i = 0; i <= n; ++i){
 		for (int j = 0; j <= w; ++j){
-		    std::cout<<c[i][j]<<" ";
+			std::cout<<c[i][j]<<" ";
 		}
 		std::cout << std::endl;
 	}
@@ -36,12 +36,13 @@ void knapsack(int n, int w){
 			if (j - weight[i] < 0){		// not enough weight in bag
 				c[i+1][j] = c[i][j];
 			} else {                    // enough weight
-				c[i+1][j] = std::max( c[i][j],
-									  /*Give up*/
-									  c[i][j - weight[i]] + value[i]
-									  /*Take current item:-weight, +value*/
-									  //c[i][j - weight[i]] is the best solution without weight[i], then add current value
-									);
+				c[i+1][j] = std::max( 
+                            c[i][j],
+                            /*Give up*/
+                            c[i][j - weight[i]] + value[i]
+                            /*Take current item:-weight, +value*/
+                            //c[i][j - weight[i]] is the best solution without weight[i], then add current value
+                            );
 			}
 			std::cout << "current n: ["<< i<<"] current w: ["<<j<<"]"<<std::endl;
 			printc(n,w);
