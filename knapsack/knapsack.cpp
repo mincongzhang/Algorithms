46f6191c907fa4ab7efe78e8d01379f25c70d601
Mincong <mincong.zhang@hotmail.com>
2016-03-07 17:54:51 +0000

    Update knapsack.cpp

--- knapsack/knapsack.cpp
+++ knapsack/knapsack.cpp
@@ -5,62 +5,61 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <vector>
 
 //http://www.csie.ntnu.edu.tw/~u91029/KnapsackProblem.html
 
 const int N = 100, W = 100000;
-int value[N], weight[N];
-int c[N + 1][W + 1];
+std::vector<int> value(N,0), weight(N,0);
+int high_value_map[N + 1][W + 1] = {};
 
-void printc(int n, int w){
-	for (int i = 0; i <= n; ++i){
-		for (int j = 0; j <= w; ++j){
-			std::cout<<c[i][j]<<" ";
-		}
-		std::cout << std::endl;
-	}
+void printHighValueMap(int n, int w){
+  for (int i = 0; i <= n; ++i){
+    for (int j = 0; j <= w; ++j){
+      std::cout<<high_value_map[i][j]<<" ";
+    }
+    std::cout << std::endl;
+  }
 
-	std::cout << std::endl;
+  std::cout << std::endl;
 }
 
 void knapsack(int n, int w){
 
-	std::cout << "item number: " << n << std::endl;
-	std::cout << "bag can carry weight: " << w << std::endl;
-	std::cout << std::endl;
+  std::cout << "item number: " << n << std::endl;
+  std::cout << "bag can carry weight: " << w << std::endl;
+  std::cout << std::endl;
 
-	memset(c, 0, sizeof(c));
-
-	for (int i = 0; i < n; ++i){     // for each item
-		for (int j = 0; j <= w; ++j){// for each weight
-			if (j - weight[i] < 0){		// not enough weight in bag
-				c[i+1][j] = c[i][j];
-			} else {                    // enough weight
-				c[i+1][j] = std::max( c[i][j],
-                                      /*Give up*/
-                                      c[i][j - weight[i]] + value[i]
-                                      /*Take current item:-weight, +value*/
-                                      //c[i][j - weight[i]] is the best solution without weight[i], then add current value
-                                      );
-			}
-			std::cout << "current n: ["<< i<<"] current w: ["<<j<<"]"<<std::endl;
-			printc(n,w);
-		}
-	}
-	std::cout << "highest value: " << c[n][w] << std::endl;
+  for (int i = 0; i < n; ++i){     // for each item
+    for (int j = 0; j <= w; ++j){  // for each weight
+      if (j - weight[i] < 0){      // not enough weight in bag
+        high_value_map[i+1][j] = high_value_map[i][j];
+      } else {                     // enough weight
+        high_value_map[i+1][j] = std::max( high_value_map[i][j],
+                              /*Give up*/
+                              high_value_map[i][j - weight[i]] + value[i]
+                              /*Take current item:-weight, +value*/
+                              //high_value_map[i][j - weight[i]] is the best solution without weight[i], then add current value
+                              );
+      }
+      std::cout << "current n: ["<<i<<"] current w: ["<<j<<"]"<<std::endl;
+      printHighValueMap(n,w);
+    }
+  }
+  std::cout << "highest value: " << high_value_map[n][w] << std::endl;
 }
 
 
 int main(){
-	value[0] = 2;
-	value[1] = 4;
-	value[2] = 5;
+  value[0] = 2;
+  value[1] = 4;
+  value[2] = 5;
 
-	weight[0] = 4;
-	weight[1] = 6;
-	weight[2] = 7;
+  weight[0] = 4;
+  weight[1] = 6;
+  weight[2] = 7;
 
-	std::cout<<"knapsack"<<std::endl;
-	knapsack(3,10);
-	system("PAUSE");
+  std::cout<<"knapsack"<<std::endl;
+  knapsack(3,10);
+  //system("PAUSE");
 }
