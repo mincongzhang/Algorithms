79967992e0ab356876e1d31e6b8a04e2c0e191fd
Mincong <mincong.zhang@hotmail.com>
2016-03-09 20:37:43 +0000

    Create kmp_edx.cpp

---
+++ KMP/kmp_edx.cpp
@@ -0,0 +1,57 @@
+#ifdef WIN32
+#include <windows>
+#endif
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+int * buildNext( char * pattern ){
+	size_t pattern_len = strlen(pattern), pattern_i = 0; 
+	int * N = new int[pattern_len];        //next[]
+	int t = N[0] = -1;           //pattern[-1] wildcard
+
+	while (pattern_i < pattern_len - 1 ){
+		if( t < 0 || pattern[pattern_i] == pattern[t] ){ //match
+			N[ ++pattern_i ] = ++t;
+		} else { //失配
+			t = N[t];
+		}
+	}
+
+	return N;
+}
+
+int match(char * text, char * pattern){
+	int * next = buildNext(pattern);
+	int text_len = (int)strlen(text), text_i = 0; 
+	int pattern_len = (int)strlen(pattern), pattern_i = 0;
+
+	std::cout<<"text:"<<text<<std::endl;
+	std::cout<<"text len:"<<text_len<<std::endl;
+	std::cout<<"pattern:"<<pattern<<std::endl;
+	std::cout<<"pattern len:"<<pattern_len<<std::endl;
+
+	while( pattern_i < pattern_len && text_i < text_len ){
+		if( pattern_i < 0 || text[text_i] == pattern[pattern_i] ){ //pattern_i<0 means get next[0] == -1
+			text_i++; pattern_i++;
+		} else {	//pattern move right, text no backout
+			pattern_i = next[pattern_i];
+		}
+	}
+
+	delete [] next;
+
+	return text_i - pattern_i;
+}
+
+int main(){
+	std::cout<<"KMP:"<<std::endl;
+
+	int m = match("abchichisnllabbba","chichinlla");
+
+	std::cout<<m<<std::endl;
+
+	system("PAUSE");
+}
