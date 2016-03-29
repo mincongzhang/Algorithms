1d133ff58c4c49bbdc82c3fa960ff8522121cb04
Mincong <mincong.zhang@hotmail.com>
2016-03-29 15:23:00 +0100

    Update main.cpp

--- PrimeNum/main.cpp
+++ PrimeNum/main.cpp
@@ -8,12 +8,47 @@
 #include <vector>
 #include <cstring>
 #include <ctime>
+#include <cmath>
 
 void windowspause(){
   #ifdef WIN32
     system("PAUSE");
   #endif
 }
+
+/*
+Input: an integer n > 1
+
+Let A be an array of Boolean values, indexed by integers 2 to n,
+initially all set to true.
+
+for i = 2, 3, 4, ..., not exceeding sqrt(n)
+ if A[i] is true:
+   for j = i^2, i^2+i, i^2+2i, i^2+3i, ..., not exceeding n
+     A[j] := false
+
+Output: all i such that A[i] is true.
+*/
+
+void generateSievePrime(const int & range){
+  std::cout<<"generateSievePrime:"<<std::endl;
+
+  std::vector<bool> integers(range,true);
+  for(int i(2); i<std::sqrt(range); ++i){
+    if(integers[i]==true){
+      for(int j(i*i); j<range; j+=i){
+        integers[j]=false;
+      }
+    }
+  }
+
+  for(int i(2); i<integers.size();++i){
+    if(integers[i]){
+      std::cout<<i<<" ";
+    }
+  }
+}
+
 
 void generatePrime(const int & range){
   std::cout<<"generatePrime:"<<std::endl;
@@ -63,7 +98,7 @@ int main(){
 
   generatePrime(range);
   generatePrime2(range);
-
+  generateSievePrime(range);
 
   windowspause();
 }
