8e4d178764c0b9b36a0154d702320cc1a2162bda
Mincong Zhang <mizhang@factset.com>
2016-04-01 14:54:06 +0100

    wordsnake

---
+++ MiniHack/word_snakes/src/word_snake.cxx
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <boost/algorithm/string.hpp>
+
+#define log(msg) do{ std::cout<<msg<<std::endl;}while(0)
+//std::ifstream infile("/home/user/jopower/londonminihacks/april2016/input_files/long.txt");
+std::ifstream infile("long.txt");
+//std::ifstream infile("medium.txt");
+const int DICT_SIZE = 26;
+
+int main(){
+  std::string line;
+  while (std::getline(infile, line)){
+    std::vector<int> dict(DICT_SIZE,0);
+
+    std::vector<std::string> words;
+    boost::split(words,line,boost::is_any_of(","));
+    for(std::vector<std::string>::iterator it = words.begin(); it != words.end(); ++it){
+      std::string word(*it);
+      dict[int(word[0]-'a')]++;
+      dict[int(word[word.size()-1]-'a')]--;
+    }
+
+  int count(0);
+  for(int i(0); i<DICT_SIZE; ++i){
+    if(dict[i] != 0){
+      count++;
+    }
+  }
+
+  if(count==2 || count==0){ log(1); }
+  else { log(0); }
+
+  }
+
+}
